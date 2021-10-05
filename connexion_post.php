d60c486981d68839eb48aa3957d17effc45b4697
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2021-10-05 13:38:08 +0200

    Fix session

--- connexion_post.php
+++ connexion_post.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 
@@ -12,50 +13,50 @@
 
 <body>   
 
-<?php
- // Connexion à la base de données
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=gbaf;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
+    <?php
+        // Connexion à la base de données
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=gbaf;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        catch (Exception $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        }
 
 
-// Données de la base de données
-$req = $bdd->prepare('SELECT * FROM users WHERE username = :username');
-$req->execute(array(
-    'username' => $_POST['username'], 
-    ));
-$resultat = $req->fetch();
+        // Données de la base de données
+        $req = $bdd->prepare('SELECT * FROM users WHERE username = :username');
+        $req->execute(array(
+            'username' => $_POST['username'], 
+            ));
+        $resultat = $req->fetch();
 
-// Comparaison du pass envoyé via le formulaire avec la base
-$isPasswordCorrect = password_verify($_POST['password'], $resultat['password']);
+        // Comparaison du pass envoyé via le formulaire avec la base
+        $isPasswordCorrect = password_verify($_POST['password'], $resultat['password']);
 
-if (!$resultat)
-{
-    // Redirection
-    header('Location: connexion.php');
-}
-else
-{
-    if ($isPasswordCorrect) {
-        session_start();
-        $_SESSION['id'] = $resultat['id'];
-        $_SESSION['name'] = $resultat['name'];
-        $_SESSION['firstname'] = $resultat['firstname'];
-        $_SESSION['username'] = $_POST['username'];
+        if (!$resultat)
+        {
+            // Redirection
+            header('Location: connexion.php');
+        }
+        else
+        {
+            if ($isPasswordCorrect) {
+                session_start();
+                $_SESSION['id'] = $resultat['id'];
+                $_SESSION['name'] = $resultat['name'];
+                $_SESSION['firstname'] = $resultat['firstname'];
+                $_SESSION['username'] = $_POST['username'];
 
-       // Redirection
-        header('Location: index.php');
-    }
-    else {
-        // Redirection
-        header('Location: connexion.php');
-    }
-}
-?>
+            // Redirection
+                header('Location: index.php');
+            }
+            else {
+                // Redirection
+                header('Location: connexion.php');
+            }
+        }
+    ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,51 +1,29 @@
+<!-- Header  -->
+<header>
+  <nav>
+    <!-- Logo-->
+    <div id="logo_GBAF">
+      <img src="images_web/logo_fonbblanc.png" alt="Logo GBAF" />
+    </div>
 
-<?php
-// On démarre la session AVANT d'écrire du code HTML
-session_start();
+    <?php 
+      if (isset($_SESSION['name']) AND isset($_SESSION['firstname'])){
+    ?>
 
-?>
+      <!-- Nom & prénom s'il y a connexion-->
+      <div id="barreDidentite">
+        <ul>     
+          <li><img src="images_web/icone_identite.png" alt="Image identite"/></li>
+          <li><a class="nomPrenom" href="parametres.php"> <?php echo (ucwords($_SESSION['firstname']). ' ' .(ucwords($_SESSION['name']))); ?> <a></li>
+        </ul>
+      </div>
 
-
-
-<!DOCTYPE html>
-<html>
-
-  <head>
-    <meta charset="utf-8"> 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Paramètre extranet GBAF</title>
-    <meta name="description" content="L'extranet pour les salariés de GBAF">
-    <link rel="stylesheet" href="styles.css"/>
-  </head>
-
-<body>     
-  <div id="container">  
-    <!-- Header  -->
-      <header>
-        <nav>
-          <!-- Logo-->
-          <div id="logo_GBAF">
-            <img src="images_web/logo_fonbblanc.png" alt="Logo GBAF" />
-          </div>
-<?php 
-if (isset($_SESSION['name']) AND isset($_SESSION['firstname'])){
-
-?>
-          <!-- Nom & prénom s'il y a connexion-->
-          <div id="barreDidentite">
-            <ul> 
-                
-              <li><img src="images_web/icone_identite.png" alt="Image identite"/></li>
-              <li><a class="nomPrenom" href="parametres.php"> <?php echo $_SESSION['name'] . ' ' . $_SESSION['firstname']; ?> <a></li>
-            </ul>
-          </div>
-          <!--Deconnexion s'il y a connexion -->
-          <div id="deconnexion">
-            <a class="deconnexion" href="connexion.php">Déconnexion <?php session_destroy()?></a>
-          </div>
-<?php          
-}
-?>
-        </nav>
-      </header>
+      <!--Deconnexion s'il y a connexion -->
+      <div id="deconnexion">
+        <a class="deconnexion" href="deconnexion.php">Déconnexion</a>
+      </div>
+    <?php          
+    }
+    ?>
+  </nav>
+</header>

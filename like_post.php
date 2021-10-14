1061f28aca43e6e1735a6f8030454e123365c6a2
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2021-10-14 14:38:44 +0200

    New name for like.php

---
+++ like_post.php
@@ -0,0 +1,11 @@
+
+<?php
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
+$_SESSION['id'] = '';
+$_SESSION['name'] = '';
+$_SESSION['firstname'] = '';
+$_SESSION['username'] = '';
+?>
+
+

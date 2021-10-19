f75e7591e8c1d56f5c4b69901bb8bf0a9939566e
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2021-10-19 13:07:54 +0200

    Update html and css

---
+++ 404.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Connexion</title>
+  <meta name="description" content="L'extranet pour les salariés de GBAF">
+  <link rel="stylesheet" href="styles.css"/>
+</head>
+
+<body>       
+  <div id="container">  
+       <!-- Header  -->
+    <?php include("header.php"); ?>
+   
+    <section id="error">
+        <p>Oups ! La page n'a pas été trouvé.</p>
+        <p>Pour retourner à l'accueil, veuillez <a href='connexion.php'>cliquer ici </a></p>
+    </section>
+          
+       
+     <?php include("footer.php"); ?>
+  </div>
+</body>
+</html>

fa1024490bc15e8130062a4ef4dd7494370d7282
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2021-10-22 14:49:46 +0200

    Update security

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 
-
 <?php 
-include("dataBaseConnection.php");
-  include("middleware.php");
+include ("middleware.php");
 
+if (isset($resultat['id'])){
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -20,68 +18,77 @@ include("dataBaseConnection.php");
   </head>
 
   <body>       
-  <div id="container">  
-       <!-- Header  -->
-    <?php include("header.php"); ?>
-   
-       <!-- Section   -->
-      <section id="entete">
-        <img src="images_web/building.jpg" alt="Image d'immeubles"/>
-        <div id="GBAF">
-          <p>Le Groupement Banque Assurance Français (GBAF) est une fédération
-            représentant les 6 grands groupes français. Le GBAF est le représentant de la profession bancaire et des assureurs sur tous
-            les axes de la réglementation financière française. Sa mission est de promouvoir
-            l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié des
-            pouvoirs publics.
-          </p>
-        </div>
-      </section> 
+    <div id="container">  
+      <!-- Header  -->
+      <?php include("header.php"); ?>
+     
+        <!-- Section   -->
+        <section id="entete">
+          <img src="images_web/building.jpg" alt="Image d'immeubles"/>
+          <div id="GBAF">
+            <p>Le Groupement Banque Assurance Français (GBAF) est une fédération
+              représentant les 6 grands groupes français. Le GBAF est le représentant de la profession bancaire et des assureurs sur tous
+              les axes de la réglementation financière française. Sa mission est de promouvoir
+              l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié des
+              pouvoirs publics.
+            </p>
+          </div>
+        </section> 
 
-      <section> <!--Code Php à inserer pour aller chercher les différents acteurs-->
-        <div id="texteActeurs">
-          <h2> Les acteurs et les partenaires</h2>
-          <p>Les produits et services bancaires sont nombreux et très variés. Afin de
-            renseigner au mieux les clients, les salariés des 340 agences des banques et
-            assurances en France (agents, chargés de clientèle, conseillers financiers, etc.)
-            recherchent sur Internet des informations portant sur des produits bancaires et
-            des financeurs, entre autres.
-            Aujourd’hui, il n’existe pas de base de données pour chercher ces informations de
-            manière fiable et rapide ou pour donner son avis sur les partenaires et acteurs du
-            secteur bancaire, tels que les associations ou les financeurs solidaires.
-            Pour remédier à cela, le GBAF souhaite proposer aux salariés des grands groupes
-            français un point d’entrée unique, répertoriant un grand nombre d’informations
-            sur les partenaires et acteurs du groupe ainsi que sur les produits et services
-            bancaires et financiers.
-            Chaque salarié pourra ainsi poster un commentaire et donner son avis
-          </p>
-        </div>
+        <section> 
+          <div id="texteActeurs">
+            <h2> Les acteurs et les partenaires</h2>
+            <p>Les produits et services bancaires sont nombreux et très variés. Afin de
+              renseigner au mieux les clients, les salariés des 340 agences des banques et
+              assurances en France (agents, chargés de clientèle, conseillers financiers, etc.)
+              recherchent sur Internet des informations portant sur des produits bancaires et
+              des financeurs, entre autres.
+              Aujourd’hui, il n’existe pas de base de données pour chercher ces informations de
+              manière fiable et rapide ou pour donner son avis sur les partenaires et acteurs du
+              secteur bancaire, tels que les associations ou les financeurs solidaires.
+              Pour remédier à cela, le GBAF souhaite proposer aux salariés des grands groupes
+              français un point d’entrée unique, répertoriant un grand nombre d’informations
+              sur les partenaires et acteurs du groupe ainsi que sur les produits et services
+              bancaires et financiers.
+              Chaque salarié pourra ainsi poster un commentaire et donner son avis
+            </p>
+          </div>
+        </section>
+
+        <section> 
+        <?php
+          // Récupération des acteurs - partenaires
+          $reponse = $bdd->query('SELECT * FROM actors');
+
+          // Affichage de chaque acteurs - partenaires 
+          while ($donnees = $reponse->fetch()){ 
+            if (isset($donnees['id'])) { 
+        ?>
+        
+              <article>
+                <img src="<?php echo(htmlspecialchars($donnees['logo']));?>" alt="Logo" />
+                <div class="paragraphe"><h3><?php echo(htmlspecialchars($donnees['actor']));?></h3>
+                  <p>
+                    <?php 
+                    $keywords = preg_split('/(?<=[.?!;:])\s+/', htmlspecialchars($donnees['description']), -1, PREG_SPLIT_NO_EMPTY);
+                    echo ($keywords[0]);
+                    ?> 
+                  </p>
+                </div>
+                <div class="lirePlus">
+                  <input onclick="window.location.href='partenaires.php?id=<?php echo($donnees['id']);?>'" class="inscription" type="submit" value="Lire la suite"/>
+                </div>
+              </article>
+            <?php
+            }
+            else {
+              // Redirection
+              header('Location: 404.php');
+            }
+            ?>
       </section>
-<section> 
-<?php
-  // Récupération des acteurs - partenaires
-  $reponse = $bdd->query('SELECT * FROM actors');
-
-  // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
-  while ($donnees = $reponse->fetch())
-  { 
-    ?>
-    <article>
-      <img src="<?php echo(htmlspecialchars($donnees['logo']));?>" alt="Logo" />
-      <div class="paragraphe"><h3><?php echo(htmlspecialchars($donnees['actor']));?></h3>
-        <p>
-          <?php 
-            $keywords = preg_split('/(?<=[.?!;:])\s+/', $donnees['description'], -1, PREG_SPLIT_NO_EMPTY);
-            echo ($keywords[0]);
-          ?> 
-        </p>
-      </div>
-      <div class="lirePlus">
-        <input onclick="window.location.href='partenaires.php?id=<?php echo(htmlspecialchars($donnees['id']));?>'" class="inscription" type="submit" value="Lire la suite"/>
-      </div>
-    </article>
-</section>
-</div>
-</body>
+    </div>
+  </body>
 </html>
 
 <?php
@@ -90,5 +97,11 @@ include("dataBaseConnection.php");
 
 include("footer.php"); 
 
+    }
+
+    else{
+       // Redirection
+ header('Location: deconnexion.php');
+    }
 
 ?>
--- middleware.php
+++ middleware.php
@@ -1,13 +1,23 @@
 <?php
-  // On démarre la session AVANT d'écrire du code HTML
-  session_start();
-  $_SESSION['id'] ;
-  $_SESSION['name'];
-  $_SESSION['firstname'] ;
-  $_SESSION['username'] ;
-  include("fonctions.php"); 
+   // On démarre la session AVANT d'écrire du code HTML
+ session_start();
+ $_SESSION['id'] ;
+ $_SESSION['name'];
+ $_SESSION['firstname'] ;
+ $_SESSION['username'] ;
+ include("fonctions.php"); 
   actualiser_session();
+ 
+ 
+ include ("dataBaseConnection.php");
 
+    // Verification des données avec la base de données
+    $req = $bdd->prepare('SELECT * FROM users WHERE id =:id ');
+    $req->execute(array(
+        'id' => $_SESSION['id'],
+        ));
+    $resultat = $req->fetch();
 
+ 
 
   ?>
